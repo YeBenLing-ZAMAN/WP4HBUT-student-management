39cdcf1bfdf564d81c8d52b428730721753db122
md kamruzzaman <kamruzzamanmd@hotmail.com>
2020-12-20 20:55:14 +0600

    book insert Module done

--- main.c
+++ main.c
@@ -1,10 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "BinarySearchTree.h"
 
-int main(){
-    ADT a;
-    BinarySearchTree* btree = new_BinarySearchTree();
-    BinaryTreeNode* n;
+int main()
+{
+
+    char option,for_add_more_book;
+    Book a;
+    BinarySearchTree *btree = new_BinarySearchTree();
+    BinaryTreeNode *n;
+    printf("\t\t\t###########################################################################");
+    printf("\n\t\t\t############                                                   ############");
+    printf("\n\t\t\t############      Library management System Project in C       ############");
+    printf("\n\t\t\t############                                                   ############");
+    printf("\n\t\t\t###########################################################################");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printf("\n\t\t\t----------------------------------------------------------------------------");
+    printf("\n\t\t\t 1.insert a new book in libary Gallery with book details ");
+    printf("\n\t\t\t 2.Remove a book for this Gallery");
+    printf("\n\t\t\t 3.Want to borrow a book from libary");
+    printf("\n\t\t\t 4.return borrowing book");
+    printf("\n\t\t\t 5.show borrow records");
+
+    printf("Enter your choice: ");
+    scanf("%c", &option);
+    switch (option)
+    {
+    case '1':   /*for insert new Book*/
+        do
+        {
+            printf("\n\t\t\tinsert a new book in libary Gallery with book details ");
+            printf("\n\t\t\tEnter your Book name ID:");
+            scanf("%d", &a.key); //a.key = 12232322;
+            printf("\n\t\t\tEnter your Book Title:");
+            scanf("%c", &a.title); //a.tille = zaman_ programming book;
+            printf("\n\t\t\tEnter your Book Author:");
+            scanf("%d", &a.key); //a.author = mamun bhai;
+            BinarySearchTree_add(btree, a);
+            printf("\n\t\t\t---------------------------------------------------------------------------\n");
+            printf("\nDo you want to add more book press 1");
+            printf("\nOr not prss: 2   ");
+            scanf("%s", &for_add_more_book);
+        } while (for_add_more_book=='1');
+
+        break;
+    case '2':
+        /* code */
+        break;
+    case '3':
+        /* code */
+        break;
+    case '4':
+        /* code */
+        break;
+    case '5':
+        /* code */
+        break;
+
+    default:
+        break;
+    }
 
     a.key = 10;
     BinarySearchTree_add(btree, a);
@@ -23,10 +77,8 @@ int main(){
 
     // delete_BinarySearchTree(btree);
 
-
-    a= BinarySearchTree_find(btree, 12);
-    printf("%d",a.key);
+    a = BinarySearchTree_find(btree, 12);
+    printf("%d", a.key);
 
     return 0;
-
 }
